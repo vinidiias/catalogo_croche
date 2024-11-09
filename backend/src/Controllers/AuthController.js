368f2d64c2b90866120c1c512d717b0668093eb6
vinidiias <vininoites8350@gmail.com>
2024-11-09 20:44:58 -0300

    att

--- backend/src/Controllers/AuthController.js
+++ backend/src/Controllers/AuthController.js
@@ -5,6 +5,7 @@ module.exports = {
     async auth(req, res) {
         const { token } = req.body
         const secretKey = process.env.SECRET_KEY
+        
         //console.log(secretKey)
         
         try{
