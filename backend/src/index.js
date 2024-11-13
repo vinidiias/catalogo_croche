a4ac013814a5a0b41574adc51a1902d832d0a43c
vinidiias <vininoites8350@gmail.com>
2024-11-13 11:27:38 -0300

    Add index.js file

---
+++ backend/src/index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const multer = require('multer')
+const path = require('path')
+const { v4: uuidv4 } = require('uuid');
+require('dotenv').config()
+const { S3Client } = require('@aws-sdk/client-s3');
+const multerS3 = require('multer-s3')
+
+const s3 = new S3Client({
+    region: 'us-east-2',
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+});
+
+const upload = multer({
+    storage: multerS3({
+        s3,
+        bucket: 'myphotoscroche',
+        key(req, file, callback) {
+            callback(null,  uuidv4() + path.extname(file.originalname))
+        }
+    })
+})
+
+/*
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: uploadsFolder,
+        filename: (req, file, callback) => callback(null, uuid() + path.extname(file.originalname))
+    })
+})
+*/
+
+const dbUri = process.env.DB_URI
+const app = express()
+const router = require('./Routes/Router')
+
+
+mongoose.connect(dbUri, {dbName:'arte-croche'})
+.then((res) => {
+    console.log('Connected to database')
+}) .catch((err) => console.log(err))
+
+app.use(cors())
+app.use(express.json({limit: '10mb'}))
+
+app.post('/file', upload.single('imagem'), (req,res) => {
+    console.log(req.file)
+})
+app.use(router)
+
+app.listen(3333, () => {
+    console.log('Serven running on port 3333')
+})
